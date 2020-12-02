33bf69969df38f68f69d2cac8552ebc79c4c1221
Marc.C <marc.cenon33@gmail.com>
2020-12-02 11:12:49 +0100

    initialisation ecran, vitesse, balle, raquette et chargement ressources

--- pong/client.py
+++ pong/client.py
@@ -16,3 +16,38 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST,PORT))
 
 #################################################
+# Taille ecran
+width = 800
+height = 600
+
+# vitesse ball
+ball_speed = [ -2, -2 ]
+
+# vitesse racket
+racket_speed_droite = [0, 0]
+racket_speed_gauche = [0, 0]
+x2 = 0
+la_balle = 0
+
+
+# Initialisation Pygame
+pygame.init()
+screen = pygame.display.set_mode( (width, height) )
+
+
+# chargement ressources
+
+background = pygame.image.load("ressources/image/background.png")
+
+ball_load = pygame.image.load("ressources/image/ball.png")
+ball_coords = ball_load.get_rect()
+
+
+racket_droite = pygame.image.load("ressources/image/racket.png")
+racket_coords_droite = racket_droite.get_rect()
+
+racket_gauche = pygame.image.load("ressources/image/racket.png")
+racket_coords_gauche = racket_gauche.get_rect()
+
+pygame.mixer.music.load('ressources/musique.wav')
+pygame.mixer.music.play()
