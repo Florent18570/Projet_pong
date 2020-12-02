bcd4ec14ea30ace8c424a250db04757510df0f48
Marc.C <marc.cenon33@gmail.com>
2020-12-02 11:10:01 +0100

    initialisation serveur

--- pong/client.py
+++ pong/client.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import sys
+import pygame
+import socket
+from pygame import mixer
+import threading
 
 
+###############################################
+# Initialisation du serveur
+# Mise en place du socket avec les protocoles IPv4 et TCP
+
+HOST = "localhost"
+PORT = 9004
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+#################################################
